c0b9f21e369b1ed6b7bf104d8bc0b436e8071470
kmj <mjkim9334@naver.com>
2017-07-18 14:14:11 +0900

    chapter 21

---
+++ chapter21/src/BoundedTypeParam.java
@@ -0,0 +1,55 @@
+/**
+ * Created by kmj on 17. 7. 18.
+ */
+interface SimpleInterface
+{
+    public void showYourName();
+}
+
+class UpperClass
+{
+    public void showYourAncestor()
+    {
+        System.out.println("UpperClass");
+    }
+}
+class AAA extends UpperClass implements SimpleInterface
+{
+    public void showYourName()
+    {
+        System.out.println("Class AAA");
+    }
+}
+
+class BBB extends UpperClass implements SimpleInterface{
+    public void showYourName()
+    {
+        System.out.println("Class BBB");
+    }
+}
+
+
+ class BoundedTypeParam {
+
+    public static <T extends SimpleInterface> void showInstanceAncestor(T param)
+    {
+        param.showYourName();
+    }
+
+    public static <T extends UpperClass> void showInstanceName(T param)
+    {
+        param.showYourAncestor();
+    }
+
+    public static void main(String[] args)
+    {
+        AAA aaa = new AAA();
+        BBB bbb = new BBB();
+
+        showInstanceAncestor(aaa);
+        showInstanceName(aaa);
+
+        showInstanceAncestor(bbb);
+        showInstanceName(bbb);
+    }
+}
---
+++ chapter21/src/IntroGenericMethod.java
@@ -0,0 +1,34 @@
+/**
+ * Created by kmj on 17. 7. 18.
+ */
+/*
+class AAA
+{
+    public String toString(){ return "Class AAA";}
+}
+
+class BBB
+{
+    public String toString(){return "Class BBB";}
+}
+
+class InstanceTypeShower2
+{
+    public <T,U> void showInstType(T inst1, U inst2)
+    {
+        System.out.println(inst1);
+        System.out.println(inst2);
+
+    }
+}
+public class IntroGenericMethod {
+    public static void main(String[] args) {
+        AAA aaa = new AAA();
+        BBB bbb = new BBB();
+
+        InstanceTypeShower2 shower = new InstanceTypeShower2();
+        shower.<AAA, BBB>showInstType(aaa, bbb);
+        shower.showInstType(aaa, bbb);
+    }
+}
+*/
