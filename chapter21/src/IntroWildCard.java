1860cf67a14ae3c81904324b1bb017678b0cc031
kmj <mjkim9334@naver.com>
2017-07-19 09:28:02 +0900

    More chapter21

---
+++ chapter21/src/IntroWildCard.java
@@ -0,0 +1,53 @@
+/**
+ * Created by kmj on 17. 7. 19.
+ */
+/*
+class Fruit
+{
+    public void showYou()
+    {
+        System.out.println("I am a Fruit.");
+    }
+}
+
+class Apple extends Fruit
+{
+    public void showYou()
+    {
+        super.showYou();
+        System.out.println("I am red");
+    }
+}
+
+class FruitBox<T> {
+    T item;
+
+    public void store(T item) {
+        this.item = item;
+    }
+
+    public T pullOut() {
+        return item;
+    }
+}
+    class IntroWildCard {
+    public static void openAndShowFruitBox(FruitBox<? extends Fruit> box)
+    {
+        Fruit fruit=box.pullOut();
+        fruit.showYou();
+    }
+
+    public static void main(String []args)
+    {
+        FruitBox<Fruit> box1=new FruitBox<Fruit>();
+        box1.store(new Fruit());
+
+        FruitBox<Apple> box2=new FruitBox<Apple>();
+        box2.store(new Apple());
+
+        openAndShowFruitBox(box1);
+        openAndShowFruitBox(box2);
+    }
+
+}
+*/
